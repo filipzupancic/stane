974472a2fc23e9af46e3bca25b750767ecb664c7
filipzupancic <filipzupancic123@gmail.com>
2022-04-14 14:54:00 +0200

    Css styles not on heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +22,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        // Resolving deploy issues
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceScheme('https');
+        }
     }
 }
