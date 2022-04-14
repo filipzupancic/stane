fbab03e224d2b5bae9056aaba211bdc77b8e085f
filipzupancic <filipzupancic123@gmail.com>
2022-04-14 17:23:41 +0200

    Added progress bar

--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="bg-white">
     <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-      <div class="text-center">
-        <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">Hello Filip</p>
-        <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Your real-estate loan is just around the corner.</p>
+      <div class="text-center radial-progress">
+        <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+          Hello, Filip
+        </p>
+        <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+          Stane ti pomaga pri vsem kar stane.
+        </p>
+
+        <div class="py-8">
+        <radial-progress-bar :diameter="300" :completed-steps="5" :total-steps="10">
+          50%
+        </radial-progress-bar>
+        </div>
       </div>
     </div>
-    <radial-progress-bar :diameter="200" :completed-steps="completedSteps" :total-steps="totalSteps">
-      <!-- Your inner content here -->
-    </radial-progress-bar>
   </div>
 </template>
 
@@ -22,8 +29,18 @@ export default defineComponent({
 
     return {
       completedSteps,
-      totalSteps
-    }
-  }
-})
+      totalSteps,
+    };
+  },
+});
 </script>
+
+<style scoped>
+.radial-progress {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+
+</style>
